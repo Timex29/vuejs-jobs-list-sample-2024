441d8aa9e47b1688ccdd8f1b358eb64e566caf2c
@Cornelio <jun23.llagas@gmail.com>
2024-08-20 15:47:07 +0800

    update base on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-crash-2024',
+  base: "/vuejs-jobs-list-sample-2024",
   server: {
     port: 3000,
 
@@ -16,7 +16,7 @@ export default defineConfig({
         target: "http://localhost:5000",
         changeOrigin: true,
         //replace localhost to /api
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
